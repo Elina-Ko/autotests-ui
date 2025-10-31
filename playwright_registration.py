7fcd784b040ab4cb9d51486766c272f8669818cd
Elina-Ko <kondratyevael@rambler.ru>
2025-10-31 15:48:17 +0300

    Работа с авторизацией в Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -27,8 +27,6 @@ with sync_playwright() as playwright:
 
     page.wait_for_timeout(1000)
 
-    from playwright.sync_api import sync_playwright
-
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state='browser-state.json')
