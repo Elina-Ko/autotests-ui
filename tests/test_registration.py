a5917acef045cb5c23e1839c90fafb438d4f6497
Elina-Ko <kondratyevael@rambler.ru>
2025-11-01 12:06:56 +0300

    Интеграция Pytest и Playwright

---
+++ tests/test_registration.py
@@ -0,0 +1,38 @@
+from playwright.sync_api import sync_playwright
+
+
+def test_successful_registration():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+        registration_email_input = page.get_by_test_id("registration-form-email-input").locator('//div//input')
+        registration_email_input.fill('user.name@gmail.com')
+
+        registration_username_input = page.get_by_test_id("registration-form-username-input").locator('//div//input')
+        registration_username_input.fill('username')
+
+        registration_password_input = page.get_by_test_id("registration-form-password-input").locator('//div//input')
+        registration_password_input.fill('password')
+
+        registration_button = page.get_by_test_id("registration-page-registration-button")
+        registration_button.click()
+
+        dashboard_header = page.get_by_test_id("dashboard-toolbar-title-text")
+
+        context.storage_state(path='browser-state.json')
+        import subprocess
+
+        subprocess.run("python -m json.tool browser-state.json > tmp.json && move tmp.json browser-state.json",
+                       shell=True)
+
+        page.wait_for_timeout(1000)
+
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context(storage_state='browser-state.json')
+        page = context.new_page()
+
+        page.wait_for_timeout(5000)
