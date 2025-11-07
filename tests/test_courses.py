0a94f3a0481a9eba285074de22ac3605250d7ad1
Elina-Ko <kondratyevael@rambler.ru>
2025-11-07 16:16:45 +0300

    Добавлена аннотация типа в test_courses

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,13 +1,11 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import expect, Page
 import pytest
 
 
 @pytest.mark.regression
 @pytest.mark.courses
-def test_empty_courses_list(chromium_page_with_state):
+def test_empty_courses_list(chromium_page_with_state: Page):
     chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-
-    chromium_page_with_state.wait_for_timeout(2000)
 
     courses_title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
     expect(courses_title).to_be_visible()
