fe4a74a88f2441dd80d76d449190f22eb8e6c8c5
Elina-Ko <kondratyevael@rambler.ru>
2025-11-14 15:12:49 +0300

    Параметризация Pytest.Практика работы с pytest параметризацией

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -4,26 +4,21 @@ import pytest
 
 @pytest.mark.regression
 @pytest.mark.authorization
+@pytest.mark.parametrize('email, password', [('user.name@gmail.com', 'password'), ('user.name@gmail.com', '  '),
+                                             ('  ', 'password')])
 def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, password: str):
     chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
-    # Находим поле с помощью get_by_test_id
-
     email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
-    email_input.fill("user.name@gmail.com")
+    email_input.fill(email)
 
-    # Находим элемент с помощью locator XPath
-
-    password_input = chromium_page.locator('//div[@data-testid="login-form-password-input"]//div//input')
-    password_input.fill("password")
-
-    # Находим поле с помощью get_by_test_id
+    password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
+    password_input.fill(password)
 
     login_button = chromium_page.get_by_test_id('login-page-login-button')
     login_button.click()
 
-    wrong_email_or_password_alert = chromium_page.locator(
-        '//div[@data-testid="login-page-wrong-email-or-password-alert"]')
+    wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
 
     expect(wrong_email_or_password_alert).to_be_visible()
     expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
